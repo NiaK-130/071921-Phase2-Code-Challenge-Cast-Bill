5df47c4f1de6935421cfdc6649257d7023337201
Nia K <nsjkappen@gmail.com>
2021-08-20 10:20:05 -0700

    submission for deadline

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BillCollection from './components/BillCollection';
 import BillsCast from './components/BillsCast';
 
+const billsAPI = "http://localhost:8002/bills";
+
 export default function App() {
-  //start here with your code for step one
+  const [bills, setBills] = useState([]);
+
+  useEffect(() => {
+    fetch(billsAPI)
+    .then((res) => res.json())
+    .then((json) => setBills(json));
+  }, []);
+
+function enlistBill(id){
+  setBills(bills.map(bill => bill.id === id? {...bill, enlisted:true} : bill));
+}
+
+function releaseBill(id){
+  setBills(bills.map(bill => bill.id === id? {...bill, enlisted:false} : bill));
+}
+
+function fireBill(id){
+  setBills(bills.filter((bill) => bill.id !==id ));
+}
 
   return (
     <div>
-      <BillsCast />
-      <BillCollection />
+      <BillsCast bills = {bills.filter(bill => bill.enlisted)} handleClick = {releaseBill} fireBill = {fireBill}/>
+      <BillCollection bills = {bills} handleClick = {enlistBill} fireBill = {fireBill} />
     </div>
   );
 }
